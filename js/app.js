3101d5572011a8171114ca8747980f4e02b85f28
Gabriel Franco <gfranco@_HOSTNAME_>
2022-05-30 16:47:39 +0200

    Comprovation, succes & errors + animation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var colorSelected;
 var random;
+var actualArray = [];
 var actualRow = 0;
 
 const totalColors = [
@@ -11,7 +12,8 @@ const totalColors = [
     '#6b4d39' // Marró
 ];
 
-const posHoritzontals = [
+const posVerticals = [
+
     '650',
     '575',
     '500',
@@ -20,15 +22,15 @@ const posHoritzontals = [
     '275',
     '200',
     '125',
-    '50'    
+    '50'
 ];
 
-const posVerticals = [
+const posHoritzontals = [
+    '50',
     '130',
     '210',
     '290',
-    '370',
-    '450'
+    '370'
 ];
 
 $(document).ready(function(){
@@ -43,21 +45,93 @@ function changeButtonColor(colorSent) {
 
     colorSelected = totalColors[colorSent];
     $('body').css('background-color', `${colorSelected}`);
+    var posActual = actualArray.length;
+    actualArray.push(colorSent);
+
+    const novaBola = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    novaBola.setAttribute("cx", posHoritzontals[posActual] );
+    novaBola.setAttribute("cy", posVerticals[actualRow] );
+    novaBola.setAttribute("r", '30');
+    novaBola.setAttribute("stroke", "black");
+    novaBola.setAttribute("stroke-width", "3");
+    novaBola.setAttribute("fill", colorSelected);
+
+    document.getElementById('mastermind').appendChild(novaBola);
 
 
+    if(actualArray.length >= 5) {
+
+        console.log(random);
+        console.log(actualArray);
+        var resultats = comprovation();
+        actualRow++;
+        actualArray = [];
+
+    }
+
+    
 
 }
 
 function generateRandom() {
-
     arrRandom = [];
+    for(let i = 0; i < 5; i++) {
+        arrRandom.push(Math.round(Math.random()*5));
+    }
+    return arrRandom;
+}
 
+function comprovation() {
+
+    var encert = 0;
+    var existeix = 0;
+
+    // Comprovació de cada posició
     for(let i = 0; i < 5; i++) {
 
-        arrRandom.push(Math.round(Math.random()*5));
+        if(random[i] == actualArray[i]) {
+            encert++;
+        }
+
+        for(let j = 0; j < 5; j++) {
+
+            if(random[i] == actualArray[j]) {
+                existeix++;
+                j = 5;
+            }
+
+        }
 
     }
 
-    return arrRandom;
+    console.log("Encerts: " + encert);
+    console.log("Existeix: " + existeix);
+
+    if(encert == 5) {
+
+        Swal.fire({        
+            type: 'success',
+            title: '¡Has guanyat el joc!',
+            showConfirmButton: false
+         }).then( (result) => {
+            location.reload();
+         });
+
+    }
+
+    if(actualRow == 8) {
+
+        Swal.fire({        
+            type: 'error',
+            title: '¡Has perdut el joc!',
+            showConfirmButton: false
+         }).then( (result) => {
+            location.reload();
+         });
+
+    }
+
+    return [encert, existeix];
+
 
 }
